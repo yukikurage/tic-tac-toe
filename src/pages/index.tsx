158f43a588894a69f1f5eb725d1717d4fd50c571
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-06-30 14:03:57 +0900

    make types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,35 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
+interface Piece {
+  readonly pieceElem: JSX.Element;
+}
+
+interface NoPiece {
+  readonly type: 'NoPiece';
+}
+
+interface SomePiece {
+  readonly type: 'SomePiece';
+  readonly piece: Piece;
+}
+
+type CellState = NoPiece | SomePiece;
+
+type Board = CellState[][];
+
+const circle: Piece = {
+  pieceElem: <span>'o'</span>,
+};
+
+const cross: Piece = {
+  pieceElem: <span>'x'</span>,
+};
+
+const initBoard: Board = new Array<CellState[]>(3).fill(
+  new Array<CellState>(3).fill({ type: 'NoPiece' }),
+);
+
 export default function Home() {
   return (
     <div className={styles.container}>

1f7613c529da31d4fd4919928e4c4df911ee68d6
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-07-01 14:12:03 +0900

    Complete!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,30 +19,126 @@ type CellState = NoPiece | SomePiece;
 
 type Board = CellState[][];
 
+type GameState = 'Game Running' | 'Game Stopped';
+
 const circle: Piece = {
-  pieceElem: <img src="/images/circle.png" className={styles.pieceImage} />,
+  pieceElem: <img src="/images/circle.png" />,
 };
 
 const cross: Piece = {
-  pieceElem: <img src="/images/cross.png" className={styles.pieceImage} />,
+  pieceElem: <img src="/images/cross.png" />,
 };
 
 const noPieceElem = <></>;
 
-const boardHeight = 3;
-const boardWidth = 3;
-const initBoard: Board = new Array(boardHeight).fill(null).map(() =>
-  new Array(boardWidth).fill(null).map(() => ({
-    type: 'NoPiece',
-  })),
-);
+const boardSize = 3;
+
+const initBoard: () => Board = () =>
+  new Array(boardSize).fill(null).map(() =>
+    new Array(boardSize).fill(null).map(() => ({
+      type: 'NoPiece',
+    })),
+  );
 
 const playerPieces = [cross, circle];
 const playerNum = playerPieces.length;
 
+const judge = (board: Board, player: number) => {
+  const targetPiece = playerPieces[player];
+  //縦方向の判定
+  for (let i = 0; i < boardSize; i++) {
+    if (
+      board.every((hori) => {
+        let cell = hori[i];
+        return cell.type === 'SomePiece' && cell.piece === targetPiece;
+      })
+    ) {
+      return true;
+    }
+  }
+  //横方向の判定
+  for (let i = 0; i < boardSize; i++) {
+    if (board[i].every((cell) => cell.type === 'SomePiece' && cell.piece === targetPiece)) {
+      return true;
+    }
+  }
+  //斜め方向の判定
+  if (
+    board.every((hori, i) => {
+      let cell = hori[i];
+      console.log(cell);
+      return cell.type === 'SomePiece' && cell.piece === targetPiece;
+    })
+  ) {
+    return true;
+  }
+  if (
+    board.every((hori, i) => {
+      let cell = hori[boardSize - 1 - i];
+      return cell.type === 'SomePiece' && cell.piece === targetPiece;
+    })
+  ) {
+    return true;
+  }
+};
+
+const judgeEveryone = (board: Board) => {
+  for (let i = 0; i < playerNum; i++) {
+    if (judge(board, i)) return i;
+  }
+  return -1;
+};
+
+const judgeNoContest = (board: Board) => {
+  for (let i = 0; i < boardSize; i++) {
+    for (let j = 0; j < boardSize; j++) {
+      if (board[i][j].type === 'NoPiece') return false;
+    }
+  }
+  return true;
+};
+
 export default function Home() {
-  const [board, setBoard] = useState(initBoard);
+  const [board, setBoard] = useState(initBoard());
   const [turn, setTurn] = useState(0);
+  const [gameState, setGameState] = useState<GameState>('Game Running');
+  const [winner, setWinner] = useState(-1);
+
+  const restartButtonClick = () => {
+    setBoard(initBoard());
+    setTurn(0);
+    setWinner(-1);
+    setGameState('Game Running');
+  };
+
+  const gameRunningMassage = () => {
+    return (
+      <div>
+        <label className={styles.turnImage}>Current Player... {playerPieces[turn].pieceElem}</label>
+      </div>
+    );
+  };
+
+  const gameStopedMassage = () => {
+    if (winner === -1) {
+      return (
+        <div>
+          <div>
+            <b> No contest </b>
+          </div>
+          <button onClick={() => restartButtonClick()}>Restart</button>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <div>
+          <label className={styles.turnImage}>Player {playerPieces[winner].pieceElem} Win!</label>
+        </div>
+        <button onClick={() => restartButtonClick()}>Restart</button>
+      </div>
+    );
+  };
 
   const updateTurn = () => {
     if (turn === playerNum - 1) {
@@ -53,10 +149,18 @@ export default function Home() {
   };
 
   const cellClick = (i: number, j: number) => {
-    if (board[i][j].type === 'NoPiece') {
+    if (board[i][j].type === 'NoPiece' && gameState === 'Game Running') {
       board[i][j] = { type: 'SomePiece', piece: playerPieces[turn] };
       setBoard(board.concat());
       updateTurn();
+    }
+    let temp_winner = judgeEveryone(board.concat());
+    if (temp_winner !== -1) {
+      setWinner(temp_winner);
+      setGameState('Game Stopped');
+    }
+    if (judgeNoContest(board)) {
+      setGameState('Game Stopped');
     }
   };
 
@@ -75,7 +179,11 @@ export default function Home() {
                 <tr key={i}>
                   {horizon.map((cell, j) => (
                     <td onClick={() => cellClick(i, j)} key={j}>
-                      {cell.type === 'SomePiece' ? cell.piece.pieceElem : noPieceElem}
+                      {cell.type === 'SomePiece' ? (
+                        <div className={styles.pieceImage}>{cell.piece.pieceElem}</div>
+                      ) : (
+                        noPieceElem
+                      )}
                     </td>
                   ))}
                 </tr>
@@ -83,8 +191,8 @@ export default function Home() {
             </tbody>
           </table>
         </div>
-        <div>
-          <b>Century Test</b>
+        <div className={styles.gameMessage}>
+          {gameState === 'Game Stopped' ? gameStopedMassage() : gameRunningMassage()}
         </div>
       </main>
 
